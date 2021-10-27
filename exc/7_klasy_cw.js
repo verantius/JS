ac764d14f124d1ae65ce6a3bb2e1c387656fc241
Adam <adam.midtt@gmail.com>
2021-10-27 18:29:08 +0200

    daily

--- exc/7_klasy_cw.js
+++ exc/7_klasy_cw.js
@@ -1,8 +1,18 @@
+function Movies (title, director, year, genre ) {
+    this.title = title
+    this.director = director
+    this.year = year
+    this.genre = genre
+}
+    Movies.prototype.getSummary = function() {
+        return `The movie ${this.title} was directed by ${this.director} in the year ${this.year} and it's classified to ${this.genre} genre`
+    }
+
+const Phantom = new Movies('Star Wars: Phantom Menace','George Lucas','2001','Science Fiction')
 
 
 
 
 
 
-
-document.getElementById('fill1').innerHTML = 1;
+document.getElementById('fill1').innerHTML = Phantom.getSummary()
