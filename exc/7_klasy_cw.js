15f0bf2da08c967ec828e53a5eb92708dace7b58
Adam <adam.midtt@gmail.com>
2021-10-27 20:08:10 +0200

    daily

--- exc/7_klasy_cw.js
+++ exc/7_klasy_cw.js
@@ -9,10 +9,31 @@ function Movies (title, director, year, genre ) {
     }
 
 const Phantom = new Movies('Star Wars: Phantom Menace','George Lucas','2001','Science Fiction')
-
-
-
-
-
-
 document.getElementById('fill1').innerHTML = Phantom.getSummary()
+
+
+class Serials {
+    constructor(subject, age, platform) {
+        this.title = subject
+        this.year = age
+        this.platform = platform
+    }
+    getPlatform = function() {
+        return `${this.platform}`
+    } 
+}
+class Serial extends Serials {
+    constructor(subject, age, platform, genre){
+        super(subject, age, platform);
+        this.genre = genre
+    }
+    getSum = function() {
+        return `You can watch ${this.title} on ${this.getPlatform()}`
+    }
+
+}
+
+
+const GoT = new Serial('Game of Thrones','2013','HBO','Fantasy')
+
+document.getElementById('fill2').innerHTML = GoT.getSum()
